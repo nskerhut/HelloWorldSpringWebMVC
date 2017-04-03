1fe5744bdbee40ae972037141ceef9e9c8f2aabc
Sebastian Skerhut <Sebastian.Skerhut@csra.com>
2017-04-03 12:45:04 -0500

    New Project

---
+++ src/main/java/com/csra/mvc/HelloWorldSpringWebMvcApplication.java
@@ -0,0 +1,12 @@
+package com.csra.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringWebMvcApplication.class, args);
+	}
+}
